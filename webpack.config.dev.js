f1f73b1bdce317d7e762f5441b1a04d01bb7435d
huangyuning <huangyn15@qq.com>
2021-12-02 15:48:08 +0800

    update: 升级依赖包

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,12 @@
-const { smart } = require('webpack-merge');
-const base = require('./webpack.config.base')
+const { merge } = require('webpack-merge');
+const base = require('./webpack.config.base');
 const path = require('path');
 
-module.exports = smart(base, {
-	output: {
-		path: path.resolve(__dirname, 'dist-dev'), //必须是绝对路径
-		filename: 'actionsAssemble.js',
-		publicPath: ''
-	},
-	devtool: 'cheap-module-eval-source-map', //开发环境下使用
-})
+module.exports = merge(base, {
+  output: {
+    path: path.resolve(__dirname, 'dist-dev'), //必须是绝对路径
+    filename: 'actionsAssemble.js',
+    publicPath: '',
+  },
+  devtool: 'cheap-module-eval-source-map', //开发环境下使用
+});
